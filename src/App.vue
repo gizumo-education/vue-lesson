5bbef5fda9a64e86c50c6757245691d03f53c0e6
Ryo Kihara <work1008.rk@gmail.com>
2023-03-01 14:36:46 +0900

    スタイルの定義

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,31 @@ export default {
 </script>
 
 <style lang="scss">
-h1 {
+.main {
+  height: 100vh;
+  background-color: rgb(250, 250, 250);
+}
+
+.section {
+  width: 60%;
+  margin: 0 auto;
+  background-color: #fff;
+  padding: 16px;
+  border-radius: 4px;
+  margin-bottom: 20px;
+  box-shadow: 0 3px 1px -2px rgb(0 0 0 / 20%), 0 2px 2px 0 rgb(0 0 0 / 14%), 0 1px 5px 0 rgb(0 0 0 / 12%);
+  &_name {
+    color: #03a9f4;
+    margin: 0;
+  }
+}
+
+.divider {
+  height: 2px;
+  background-color: #03a9f4;
+}
+
+.green {
   color: green;
 }
 </style>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
+import './scss/app.scss'
 
 new Vue({
   el: '#app',
